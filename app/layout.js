a4467708380b3ffaa32f5dc9c161f257261be282
oyaruchyk <yaruchyk.o@gmail.com>
2025-11-28 16:14:39 +0100

    google tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,9 +17,28 @@ export const metadata = {
   description: "Experience the art of giving. Organize sophisticated Secret Santa events with ease.",
 };
 
+// Use the provided Google Analytics ID
+const GA_TRACKING_ID = "G-V02C9MJH5S";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
       <body className={`${inter.variable} ${playfair.variable} antialiased`}>
         {children}
       </body>
